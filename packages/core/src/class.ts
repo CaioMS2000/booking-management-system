456b5400bc4013207b758fc76e5cdb80c4575d58
Caio Marques Silva <caiocpe@gmail.com>
2025-12-22 12:07:15 -0300

    feat(core): add TestClass and ComplexClass with unit tests for resource handling

--- packages/core/src/class.ts
+++ packages/core/src/class.ts
@@ -5,29 +5,3 @@ export type Resources = {
 export abstract class Class<T extends Resources> {
 	protected abstract readonly resources: T
 }
-
-function printIt(x: any) {
-	console.log(x)
-}
-
-type SomeClassResources = {
-	someProperty: string
-	someFunction: (...args: any[]) => void
-}
-
-class SomeClass extends Class<SomeClassResources> {
-	constructor(protected readonly resources: SomeClassResources) {
-		super()
-	}
-	get someProperty() {
-		return this.resources.someProperty
-	}
-
-	someFunction(...args: any[]) {
-		return this.resources.someFunction(...args)
-	}
-}
-const x = new SomeClass({
-	someProperty: 'Any string value, does not matter what',
-	someFunction: printIt,
-})
