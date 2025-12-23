8be18db14b53f4589409e9d5ee907ddb5eda4186
Caio Marques Silva <caiocpe@gmail.com>
2025-12-23 16:11:08 -0300

    feat(property-system): update package.json dependencies and add Property entity

---
+++ apps/property-system/src/domain/entities/property.ts
@@ -0,0 +1,5 @@
+import { Entity } from '@repo/core'
+
+type PropertyProps = {}
+
+export class Property extends Entity<PropertyProps> {}
---
+++ packages/core/src/entity/index.ts
@@ -0,0 +1,3 @@
+export * from './aggregate-root'
+export * from './entity'
+export * from './unique-entity-id'
