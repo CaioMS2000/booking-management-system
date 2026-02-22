c48501773417a7445a3b1db5aa980db257d96534
Caio Marques Silva <caiocpe@gmail.com>
2026-02-22 14:38:12 -0300

    feat(booking-module): add Reservation and Guest domain models
    Introduce the core domain entities for the booking module,
    including factory methods with auto-generated IDs via appContext.

---
+++ apps/property-management/src/modules/booking-module/domain/models/guest.ts
@@ -0,0 +1,37 @@
+import { Class, Email, Name, Phone, UniqueId } from '@repo/core'
+import { appContext } from '@/application-context'
+
+export type GuestProps = {
+	id: UniqueId
+	name: Name
+	email: Email
+	phone: Phone
+}
+
+export type GuestCreateInput = Omit<GuestProps, 'id'>
+
+export class Guest extends Class<GuestProps> {
+	constructor(protected readonly props: GuestProps) {
+		super()
+	}
+
+	get id() {
+		return this.props.id
+	}
+
+	static async create(input: GuestCreateInput, id?: UniqueId) {
+		const { name, email, phone } = input
+
+		if (!id) {
+			const context = appContext.get()
+			id = await context.idGenerator.V7.generate()
+		}
+
+		return new Guest({
+			id,
+			name,
+			email,
+			phone,
+		})
+	}
+}
---
+++ apps/property-management/src/modules/booking-module/domain/models/reservation.ts
@@ -0,0 +1,78 @@
+import {
+	Class,
+	Money,
+	Optional,
+	ReservationPeriod,
+	ReservationStatus,
+	UniqueId,
+} from '@repo/core'
+import { appContext } from '@/application-context'
+
+export type ReservationProps = {
+	id: UniqueId
+	listingId: UniqueId
+	guestId: UniqueId
+	period: ReservationPeriod
+	status: ReservationStatus
+	totalPrice: Money
+}
+
+export type ReservationCreateInput = Omit<
+	Optional<ReservationProps, 'status'>,
+	'id'
+>
+
+export class Reservation extends Class<ReservationProps> {
+	constructor(protected readonly props: ReservationProps) {
+		super()
+	}
+
+	get id() {
+		return this.props.id
+	}
+
+	get period() {
+		return this.props.period
+	}
+
+	get status() {
+		return this.props.status
+	}
+
+	get totalPrice() {
+		return this.props.totalPrice
+	}
+
+	get listingId() {
+		return this.props.listingId
+	}
+
+	get guestId() {
+		return this.props.guestId
+	}
+
+	cancel(): Reservation {
+		return new Reservation({
+			...this.props,
+			status: 'CANCELLED',
+		})
+	}
+
+	static async create(input: ReservationCreateInput, id?: UniqueId) {
+		const { guestId, period, listingId, totalPrice, status = 'PENDING' } = input
+
+		if (!id) {
+			const context = appContext.get()
+			id = await context.idGenerator.V7.generate()
+		}
+
+		return new Reservation({
+			id,
+			guestId,
+			period,
+			listingId,
+			totalPrice,
+			status,
+		})
+	}
+}
