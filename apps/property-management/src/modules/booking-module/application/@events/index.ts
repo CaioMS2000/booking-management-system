fddbbd48409628b816529871d212f8117ddcef69
Caio Marques Silva <caiocpe@gmail.com>
2026-02-26 18:37:42 -0300

    chore: remove source files after rename and event extraction
    Removes the original @events/ files from the booking-module app layer
    and the entire packages/modules-contracts package, which were already
    relocated to @repo/shared in previous commits.

--- apps/property-management/src/modules/booking-module/application/@events/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './reservation-created-event'
-export * from './reservation-confirmed-event'
-export * from './reservation-cancelled-event'
-export * from './reservation-completed-event'
