52236ca7bbe69ecbbe2e2c330ecf8d63b0614c46
Caio Marques Silva <caiocpe@gmail.com>
2026-02-28 12:08:30 -0300

    refactor(booking-module): migrate DI registrations to awilix
    Delete BOOKING_MODULE_TOKENS symbols and re-register EventBus using
    awilix asFunction singleton.

--- apps/property-management/src/modules/booking-module/init.ts
+++ apps/property-management/src/modules/booking-module/init.ts
@@ -1,6 +1,7 @@
 import { EventBus } from '@repo/core'
-import { BOOKING_MODULE_TOKENS } from './tokens'
+import { asFunction } from 'awilix'
+import { container } from '@/container'
 
-// container.registerSingleton(PROPERTY_MODULE_TOKENS.QueryBus, QueryBus)
-// container.registerSingleton(PROPERTY_MODULE_TOKENS.CommandBus, CommandBus)
-container.registerSingleton(BOOKING_MODULE_TOKENS.EventBus, EventBus)
+container.register({
+	eventBus: asFunction(() => new EventBus()).singleton(),
+})
--- apps/property-management/src/modules/booking-module/tokens.ts
+++
@@ -1,6 +0,0 @@
-import { EventBus } from '@repo/core'
-
-export const BOOKING_MODULE_TOKENS = {
-	// IdGeneratorV4: Symbol('IdGeneratorV4') as InjectionToken<IdGenerator>,
-	EventBus: Symbol('EventBus') as InjectionToken<EventBus>,
-} as const
